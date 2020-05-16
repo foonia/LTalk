dbcf76c30decaed17c481a63bfe3ecd290e28e0a
foonia <ljh9032a@naver.com>
2020-05-16 16:24:46 +0900

    Add login/main page

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template, redirect, url_for
+from flask_socketio import SocketIO, send
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return redirect('login')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/main')
+def main():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('main.html')
+    else:
+        return redirect(url_for('index'))
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: {}'.format(msg))
+    send(msg, broadcast=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
