f9da422fc758139c71c10e2761b368bdd54270e7
Acapellia <longchim@naver.com>
2020-05-19 17:05:20 +0900

    enter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_socketio import SocketIO, send
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
@@ -35,3 +36,4 @@ def handle_message(msg):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
