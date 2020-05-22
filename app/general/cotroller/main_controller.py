3b9c82dd406a0387efd2943001dd187454aadec7
foonia <ljh9032a@naver.com>
2020-05-22 12:45:50 +0900

    fixed issue

---
+++ app/general/cotroller/main_controller.py
@@ -0,0 +1,66 @@
+from flask import (
+    Blueprint, render_template, request, session,
+    g, redirect, url_for
+)
+from flask_socketio import send, join_room
+
+from ..model.User import User
+
+
+general_bp = Blueprint('general_bp', __name__)
+
+###############################################################
+users = []
+users.append(User(id=1, username='Acapellia', password=''))
+users.append(User(id=2, username='a', password=''))
+users.append(User(id=3, username='b', password=''))
+###############################################################
+
+
+@general_bp.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+
+@general_bp.route('/')
+def index():
+    return redirect('login')
+
+
+@general_bp.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@general_bp.route("/profile")
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+
+
+@general_bp.route("/insertroom")
+def chat():
+    username = 'guest'
+    room = request.args.get('room')
+    if room:
+        return render_template('main.html', username=username, room=room)
+    else:
+        return redirect(url_for('login'))
+
--- manage.py
+++ manage.py
@@ -5,15 +5,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_socketio import SocketIO
 
-from app.main import create_app, db
-from app.main.cotroller.main_controller import main
+from app import create_app, db
 
 app = create_app(os.getenv('FLASK_ENV') or 'dev')
 # app.app_context().push()
 manager = Manager(app)
 # migrate = Migrate(app, db)
 # manager.add_command('db', MigrateCommand)
-app.register_blueprint(main)
+
 socketio = SocketIO(app)
 
 
