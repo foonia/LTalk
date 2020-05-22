4df5eb89f4e54c01ed5f4a619cbac45f682c61f4
Acapellia <longchim@naver.com>
2020-05-22 22:36:22 +0900

    ui login->main & temp use cookie

--- app/general/cotroller/main_controller.py
+++ app/general/cotroller/main_controller.py
@@ -1,6 +1,6 @@
 from flask import (
     Blueprint, render_template, request, session,
-    g, redirect, url_for
+    g, redirect, url_for, make_response
 )
 from flask_socketio import send, join_room
 
@@ -19,9 +19,10 @@ users.append(User(id=3, username='b', password=''))
 
 @general_bp.before_request
 def before_request():
+    CUser = request.cookies.get('username')
     g.user = None
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
+    if 'username' in session:
+        user = [x for x in users if x.id == session[CUser]][0]
         g.user = user
 
 
@@ -32,35 +33,51 @@ def index():
 
 @general_bp.route("/login", methods=['GET', 'POST'])
 def login():
+    resp = make_response(redirect(url_for('general_bp.chat')))
     if request.method == 'POST':
         session.pop('user_id', None)
 
         username = request.form['username']
-        password = request.form['password']
+        room = request.form['room']
+
+
+        resp.set_cookie('username',username)
+        resp.set_cookie('room','1')
+
 
         user = [x for x in users if x.username == username][0]
+        '''
         if user and user.password == password:
-            session['user_id'] = user.id
+            session[username] = user.username
             return redirect(url_for('profile'))
+        '''
+        if user and room:
+            session[username] = user.username
+            return resp
         return redirect(url_for('login'))
+
 
     return render_template('login.html')
 
-
+'''
 @general_bp.route("/profile")
 def profile():
     if not g.user:
         return redirect(url_for('login'))
-
     return render_template('profile.html')
+'''
 
 
 @general_bp.route("/insertroom")
 def chat():
-    username = 'guest'
+    username = g.user
+    '''
     room = request.args.get('room')
+    '''
+    room = request.cookies.get('room')
+
     if room:
         return render_template('main.html', username=username, room=room)
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for('general_bp.login'))
 
